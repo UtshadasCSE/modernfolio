0627982cfb8acd1859f08e6e0908edee4a4bac91
UtshadasCSE <utshachandradas@gmail.com>
2025-11-10 22:23:27 +0600

    Footer margin fixing

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 
 const Footer = () => {
   return (
-    <div id="contact" className="pt-30px">
+    <div id="contact" className="pt-30px mt-30px">
       <Card className="p-3">
         <CardTitle className="text-muted-foreground text-sm font-light text-center">
           Best way to reach me is through:
